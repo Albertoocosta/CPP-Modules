1366480d1eb04ad3f1a82b3fc0b18479c31a13f7
Carlos Alberto Da Fonseca Costa Neto <cda-fons@c2r4s3.42porto.com>
2025-10-28 18:57:21 +0000

    CPP03/ex01

---
+++ CPP03/ex01/ScavTrap.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 14:29:12 by cda-fons          #+#    #+#             */
+/*   Updated: 2025/10/28 15:58:14 by cda-fons         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    ScavTrap();
+    ScavTrap(std::string name);
+    ScavTrap(const ScavTrap& src);
+    ScavTrap& operator=(const ScavTrap& src);
+    ~ScavTrap();
+
+    void attack(const std::string& target);
+
+    void guardGate(void);
+};
+
+#endif
